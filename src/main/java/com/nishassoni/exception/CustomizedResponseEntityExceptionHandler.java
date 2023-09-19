4e4dc868621c11ba90925ec4d5fc4f5d847ea889
Soni Nisha <soni.nishaa24@gmail.com>
2023-09-19 22:43:03 +0530

    19 Sept

--- src/main/java/com/nishassoni/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/nishassoni/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.nishassoni.exception;
 
 
-import java.time.LocalDate;
+
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -17,16 +20,30 @@ import com.nishassoni.user.UserNotFoundException;
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request){
+	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request){
 	ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 	
-	return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(UserNotFoundException.class)
-	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request){
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request){
 	ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 	
-	return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
+	return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 	}
+	//ResponseEntityExceptionHandler inside i get 
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), 
+		"Total Errors: " + ex.getErrorCount()+ "First Errors: " +ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+		
+		
+		
+		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+		
+		
+	}
+
 }
--- src/main/java/com/nishassoni/user/User.java
+++ src/main/java/com/nishassoni/user/User.java
@@ -2,13 +2,15 @@ package com.nishassoni.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
 	
 	private Integer id;
-	@Size(min = 2)
+	@Size(min = 2, message="Name should have atleast 2 character")
 	private String name;
+	@Past(message = "Birth Date should be Past")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
